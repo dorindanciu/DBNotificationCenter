f516c86a32a9d98cf2e91eac3b1464ffa87c8203
Dorin Danciu <dorindanciu@Dorins-MacBook-Pro.local>
2012-10-07 13:20:44 +0300

    Initial Commit

---
+++ DBNotificationCenter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DBNotificationCenter
+//
+//  Created by Dorin Danciu on 10/7/12.
+//  Copyright (c) 2012 Dorin Danciu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DBNotificationCenter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DBNotificationCenter
+//
+//  Created by Dorin Danciu on 10/7/12.
+//  Copyright (c) 2012 Dorin Danciu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
